f37b89ebc7fe68dc13ba3e980b70c9078cbb96e2
insubu <132106829+insubu@users.noreply.github.com>
2025-07-29 11:16:42 +0900

    Update timeRemaining.sql

--- timeRemaining.sql
+++ timeRemaining.sql
@@ -13,3 +13,10 @@ JOIN
   v$session_longops l ON s.sid = l.sid AND s.serial# = l.serial#
 WHERE
   l.time_remaining > 0;
+
+BEGIN
+  DBMS_METADATA.set_transform_param(DBMS_METADATA.session_transform, 'STORAGE', false);
+  DBMS_METADATA.set_transform_param(DBMS_METADATA.session_transform, 'TABLESPACE', false);
+  DBMS_METADATA.set_transform_param(DBMS_METADATA.session_transform, 'SEGMENT_ATTRIBUTES', false);
+END;
+
